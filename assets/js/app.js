674daeda690764ae7e484a8d2ef28bdd098d9f84
VeRocha93 <vero_loveh@hotmail.com>
2025-05-05 00:00:36 -0600

    Actualizacion a LS

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,24 @@
 console.log("Prueba de scrip de la pagina de JS");
+
+//Local storage = almacenamiento interno
+localStorage.setItem("Fecha","05 Mayo")
+localStorage.setItem("Turno","Primero")
+
+//Colocar el valor del item seleccionado
+const titulo = document.getElementById("ValorDeLocalStorage")
+titulo.innerHTML = localStorage.getItem("Turno")
+
+//Insertar botton borrar item LS
+const botonBorrar = document.getElementById("btnBorrarLS");
+botonBorrar.addEventListener("click", ()=> {
+    localStorage.removeItem("Turno");
+    titulo.innerHTML = "Elemento eliminado";
+});
+
+//Insertar botton borrar todo LS
+const botonBorrarTodo = document.getElementById("btnBorrarTodoLS");
+botonBorrarTodo.addEventListener("click", ()=> {
+    localStorage.clear();
+    titulo.innerHTML = "Local ST Eliminado";
+});
+
