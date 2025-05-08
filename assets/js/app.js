35c709c49e4d91bbdb619397de066ac95f2543de
VeRocha93 <vero_loveh@hotmail.com>
2025-05-07 22:57:31 -0600

    Se agrega Comentarios del equipo  y persona que recibio equipo

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,7 +2,9 @@
 let equipos = JSON.parse(localStorage.getItem("equipos")) || [];
 
 // 2.- DOM: Sirve para conectar HTML con JS 
-const inputEquipo = document.getElementById("equipo");
+const inputSerial = document.getElementById("equipo");
+const inputCaracteristicas = document.getElementById("caracteristicas");
+const inputPersona = document.getElementById("persona");
 const btnRegistrar = document.getElementById("btn-registrar");
 const listaEquipos = document.getElementById("lista-equipos");
 
@@ -14,8 +16,12 @@ function mostrarEquipos() {
     li.className =
       "list-group-item d-flex justify-content-between align-items-center";
     li.innerHTML = `
-      <span>${equipo}</span>
       <div>
+        <strong>Serial:</strong> ${equipo.serial}<br>
+        <strong>Características:</strong> ${equipo.caracteristicas}<br>
+        <strong>Persona:</strong> ${equipo.persona}
+      </div>
+      <div class="mt-2">
         <button class="btn btn-warning btn-sm me-2" onclick="editarEquipo(${index})">Editar</button>
         <button class="btn btn-danger btn-sm" onclick="borrarEquipo(${index})">Borrar</button>
       </div>
@@ -24,44 +30,56 @@ function mostrarEquipos() {
   });
 }
 
-// 4.- Agregar nuevo equipo
+// 4.- Agregar nuevo equipo reviar *******
 function agregarEquipo() {
-  const equipo = inputEquipo.value.trim();
-  if (equipo === "") {
-    alert("Por favor, ingresa un serial de equipo.");
-    return;
-  }
-  equipos.push(equipo);
-  localStorage.setItem("equipos", JSON.stringify(equipos));
-  inputEquipo.value = "";
+    const serial = document.getElementById("serial").value.trim();
+    const caracteristicas = document.getElementById("caracteristicas").value.trim();
+    const persona = document.getElementById("persona").value.trim();
+  
+    if (!serial || !caracteristicas || !persona) {
+      alert("Por favor, No dejes espacios vacios.");
+      return;
+    }
+    const nuevoEquipo = {
+        serial,
+        caracteristicas,
+        persona
+      };
+    
+      if (modoEditar) {
+        equipos[indexEditar] = nuevoEquipo;
+        btnRegistrar.innerText = "Agregar equipo";
+        modoEditar = false;
+        indexEditar = null;
+      } else {
+        equipos.push(nuevoEquipo);
+     }
+     localStorage.setItem("equipos", JSON.stringify(equipos));
+  limpiarCampos();
   mostrarEquipos();
 }
 
 // 5.- Borrar equipo
 function borrarEquipo(index) {
-  equipos.splice(index, 1);
-  localStorage.setItem("equipos", JSON.stringify(equipos));
-  mostrarEquipos();
-}
+    document.getElementById("serial").value = "";
+    document.getElementById("caracteristicas").value = "";
+    document.getElementById("persona").value = "";
+  
+    mostrarEquipos();
+  }
 
 // 6.- Editar equipo
 function editarEquipo(index) {
-  inputEquipo.value = equipos[index];
-  btnRegistrar.innerText = "Actualizar";
-  btnRegistrar.onclick = function () {
-    const equipoActualizado = inputEquipo.value.trim();
-    if (equipoActualizado === "") {
-      alert("Por favor, ingresa tu cambio de equipo.");
-      return;
-    }
-    equipos[index] = equipoActualizado;
-    localStorage.setItem("equipos", JSON.stringify(equipos));
-    inputEquipo.value = "";
-    btnRegistrar.innerText = "Agregar equipo";
-    btnRegistrar.onclick = agregarEquipo;
-    mostrarEquipos();
-  };
-}
+    const equipo = equipos[index];
+    inputSerial.value = equipo.serial;
+    inputCaracteristicas.value = equipo.caracteristicas;
+    inputPersona.value = equipo.persona;
+  
+    btnRegistrar.innerText = "Actualizar equipo";
+    modoEditar = true;
+    indexEditar = index;
+  }
 
-// Mostrar equipos al cargar la página
-mostrarEquipos();
+  
+  // Inicializar
+  mostrarEquipos();
